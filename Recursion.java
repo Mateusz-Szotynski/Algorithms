d92c2a2cc0a4e239c0359c724b9e12fd34da3ca8
Mateusz <mateusz.szotynski3@gmail.com>
2024-12-14 17:27:34 +0100

    Created recursive method

---
+++ Recursion.java
@@ -0,0 +1,12 @@
+public class Recursion {
+    public static void main(String ... args) {
+        countDown(10);
+    }
+
+    public static void countDown(int number) {
+        if (number >= 0) {
+            System.out.println(number);
+            countDown(number - 1);
+        }
+    }
+}
